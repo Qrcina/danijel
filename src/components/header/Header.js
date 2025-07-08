0596c5fe7ffc40662f5671acfd446b345bba7fea
danijel89-spec <thegolub1989@gmail.com>
2025-07-08 19:14:26 +0200

    Fix code formatting with Prettier

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import Headroom from "react-headroom";
 import "./Header.scss";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
@@ -18,7 +18,7 @@ import {
 import logo from "../../assets/images/Tartu_Logo.png";
 
 function Header() {
-  const { isDark } = useContext(StyleContext);
+  const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
   const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
@@ -33,9 +33,9 @@ function Header() {
         <a
           href="/"
           className="logo"
-          style={{ display: "flex", alignItems: "center", gap: "8px" }}
+          style={{display: "flex", alignItems: "center", gap: "8px"}}
         >
-          <img src={logo} alt="Tartu Logo" style={{ height: 30 }} />
+          <img src={logo} alt="Tartu Logo" style={{height: 30}} />
           <span className="grey-color"> &lt;</span>
           <span className="logo-name">{greeting.username}</span>
           <span className="grey-color">/&gt;</span>
@@ -45,7 +45,7 @@ function Header() {
         <label
           className="menu-icon"
           htmlFor="menu-btn"
-          style={{ color: "white" }}
+          style={{color: "white"}}
         >
           <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
         </label>
